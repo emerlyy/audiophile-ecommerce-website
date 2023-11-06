bc4e4ea1bc9681a68c0232b988e0b658c8ade54f
Emerlyy <vadim200339@gmail.com>
2023-11-06 22:58:53 +0200

    created HomePage component

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header/Header";
+import Hero from "@/sections/Hero/Hero";
+
+const HomePage = () => {
+	return (
+		<>
+			<Header />
+			<Hero />
+		</>
+	);
+};
+
+export default HomePage;
