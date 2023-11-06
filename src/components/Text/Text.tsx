de036c62e7be6849b2371a82125e9bc9150d0d29
Emerlyy <vadim200339@gmail.com>
2023-11-06 22:57:03 +0200

    Created Text Component

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,32 @@
+import styles from "./Text.module.css";
+
+type TextColor = "dark" | "light";
+
+type TextProps = {
+	color?: TextColor;
+	extraClasses?: string;
+	children: React.ReactNode;
+};
+
+const colors: {
+	[key in TextColor]: string;
+} = {
+	dark: styles["text-dark"],
+	light: styles["text-light"],
+};
+
+const getStyles = (color: TextColor, extra?: string) => {
+	let textStyles = `${styles.text} ${colors[color]}`;
+
+	if (extra) {
+		textStyles += ` ${extra}`;
+	}
+
+	return textStyles;
+};
+
+const Text = ({ color = "dark", extraClasses, children }: TextProps) => {
+	return <p className={getStyles(color, extraClasses)}>{children}</p>;
+};
+
+export default Text;
