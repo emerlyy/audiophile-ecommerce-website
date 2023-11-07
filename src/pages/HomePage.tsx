9bad0ee2029848c668810afa6f8e519a66d24f42
Emerlyy <vadim200339@gmail.com>
2023-11-07 13:42:42 +0200

    added sections on home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,23 @@
+import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
+import BestGear from "@/sections/BestGear/BestGear";
+import Categories from "@/sections/Categories/Categories";
 import Hero from "@/sections/Hero/Hero";
+import YX1Earphones from "@/sections/YX1Earphones/YX1Earphones";
+import ZX7Speaker from "@/sections/ZX7Speaker/ZX7Speaker";
+import ZX9Speaker from "@/sections/ZX9Speaker/ZX9Speaker";
 
 const HomePage = () => {
 	return (
 		<>
 			<Header />
 			<Hero />
+			<Categories />
+			<ZX9Speaker />
+			<ZX7Speaker />
+			<YX1Earphones />
+			<BestGear />
+			<Footer />
 		</>
 	);
 };
