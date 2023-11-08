be12b2438ae9686e31a99ed7c509cab4457218ce
Emerlyy <vadim200339@gmail.com>
2023-11-08 21:56:46 +0200

    changed build out directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
-import { defineConfig } from "vitest/config"
-import react from "@vitejs/plugin-react"
-import path from "path"
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  },
-  server: {
-    open: true,
-  },
-  build: {
-    outDir: "build",
-    sourcemap: true,
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
-    mockReset: true,
-  },
-})
+	plugins: [react()],
+	resolve: {
+		alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+	},
+	server: {
+		open: true,
+	},
+	build: {
+		outDir: "dist",
+		sourcemap: true,
+	},
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: "src/setupTests",
+		mockReset: true,
+	},
+});
